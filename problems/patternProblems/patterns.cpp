5b07726aa239540e3b1a2d58005414ae1434a1fc
MObinXIV <mobinxiv@gmail.com>
2023-06-27 06:09:10 +0300

    do some refactor

--- problems/patternProblems/patterns.cpp
+++ problems/patternProblems/patterns.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 
 using namespace std;
+void pattern1( int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+            cout << i;
+        cout << endl;
+    }
+}
 
+void pattern2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // Space
+        for (int j = i; j < n; j++)
+            cout << " ";
+
+        // Stars
+        for (int k = 0; k < i * 2 + 1; k++)
+            cout << "*";
+
+        // Space
+        for (int j = 0; j < n; j++)
+            cout << " ";
+        cout << endl;
+    }
+}
 int main()
 {
     int t; 
@@ -9,6 +36,6 @@ int main()
 
     for (int i =0;i<t;i++)
     {
-        
+
     }
 }
