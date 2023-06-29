db962a2a5612d02f92816d9455d88d394f085a05
MObinXIV <mobinxiv@gmail.com>
2023-06-29 19:22:05 +0300

    add logarithm nice way

--- problems/mathProblems/countDigits.cpp
+++ problems/mathProblems/countDigits.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -6,11 +6,13 @@ int main()
 {
     int n;
     cin>>n;
-    int cnt=0;
-   while(n>0)
-   {
-    n/=10;
-    cnt++;
-   }
+//     int cnt=0;
+//    while(n>0)
+//    {
+//     n/=10;
+//     cnt++;
+//    }
+        cout <<int(log10(n))<<endl;
+        int cnt = int (log10(n)+1);
    cout<<cnt;
 }
