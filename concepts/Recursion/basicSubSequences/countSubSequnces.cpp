d8e710dec37bf9c4c45d603331af9a0357f101ce
MObinXIV <mobinxiv@gmail.com>
2023-07-26 08:13:20 +0300

    delete uncessary values

--- concepts/Recursion/basicSubSequences/countSubSequnces.cpp
+++ concepts/Recursion/basicSubSequences/countSubSequnces.cpp
@@ -1,34 +1,30 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int subsetSum(int ind, vector<int> &ds, int s, int sum, vector<int> &nums, vector<vector<int>> &ans, int n)
+int subsetSum(int ind,  int s, int sum, vector<int> &nums, int n)
 {
     if (ind == n)
     {
         if (s == sum)
         {
-            ans.push_back(ds);
             return 1;
         }
         return 0;
     }
 
-    ds.push_back(nums[ind]);
+   
     s += nums[ind];
-    int l= subsetSum(ind + 1, ds, s, sum, nums, ans, n) ; // count the left recursion calls
+    int l= subsetSum(ind + 1, s, sum, nums,  n) ; // count the left recursion calls
         
     s -= nums[ind];
-    ds.pop_back();
-    int r = subsetSum(ind + 1, ds, s, sum, nums, ans, n); // count the right recursion calls
+    int r = subsetSum(ind + 1,  s, sum, nums,  n); // count the right recursion calls
 
     // after U finish give the left guy with right guy
     return l+r;
 }
 int subsetSum(vector<int> &nums, int sum)
 {
-    vector<vector<int>> ans;
-    vector<int> ds;
-    int res=subsetSum(0, ds, 0, sum, nums, ans, nums.size());
+    int res=subsetSum(0,  0, sum, nums,  nums.size());
     return res;
 }
 int main()
