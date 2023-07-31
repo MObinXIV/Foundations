a605b9e17220c0759b5adb79e25525ec973460a4
MObinXIV <mobinxiv@gmail.com>
2023-07-31 10:26:54 +0300

    hashing with hashmap for the purpose of size greater than 1e9

---
+++ concepts/Hashing/hashMap.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+
+{
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i =0;i<n;i++)
+    cin>>arr[i];
+
+    //pre-compute
+    map<int,int>mpp;
+    for(int i =0;i<n;i++)
+    mpp[arr[i]]++;
+
+    int q;
+    cout<<"Enter the number of queries U wanna execute\n";
+    cin>>q;
+    while (q--)
+    {
+        int num;
+        cout<<"Enter a number U wanna see it's frequency\n";
+        cin>>num;
+        cout<<mpp[num];
+    }
+    
+
+}
