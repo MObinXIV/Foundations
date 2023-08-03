92b200f40d21cfa4b4499f4566a4a014a5b53fda
MObinXIV <mobinxiv@gmail.com>
2023-08-03 12:04:24 +0300

    optimize by add a variable to stop the loop if the array get sorted

--- concepts/Sorting/elementaryMethods/bubbleSort.cpp
+++ concepts/Sorting/elementaryMethods/bubbleSort.cpp
@@ -8,11 +8,16 @@ void bubble_Sort(vector<int> &arr)
     int n = arr.size();
     for (int i = n - 1; i > 0; i--)
     {
+        int didSwap=0;
         for (int j = 0; j < i; j++)
         {
             if (arr[j] > arr[j + 1])
-                swap(arr[j], arr[j + 1]);
+               { swap(arr[j], arr[j + 1]);
+                didSwap=1;
+               }
         }
+        if(didSwap==0)
+        break;
     }
 }
 int main()
