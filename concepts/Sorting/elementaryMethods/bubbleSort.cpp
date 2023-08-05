b03f3b63048eaa884a68f6dc57ca92b9e5da1373
MObinXIV <mobinxiv@gmail.com>
2023-08-05 21:42:29 +0300

    add recursive bubblesort and do some transport

--- concepts/Sorting/elementaryMethods/bubbleSort.cpp
+++ concepts/Sorting/elementaryMethods/bubbleSort.cpp
@@ -20,3 +20,22 @@ void bubble_Sort(vector<int> &arr)
         break;
     }
 }
+void recursiveBubble(int n, vector<int> &arr,bool didSwap)
+{
+    if (n == 1 || !didSwap)
+    {
+        return;
+    }
+
+    // code recursive step
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+        swap(arr[i], arr[i + 1]);
+        didSwap = 1;
+        }
+    }
+    
+        recursiveBubble(n - 1, arr, didSwap);
+}
--- tst.cpp
+++ tst.cpp
@@ -1,4 +1,33 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+void recursiveBubble(int n, vector<int> &arr, bool didSwap)
+{
+    if (n == 1 || !didSwap)
+    {
+        return;
+    }
+
+    // code recursive step
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            swap(arr[i], arr[i + 1]);
+            didSwap = 1;
+        }
+    }
+
+    recursiveBubble(n - 1, arr, didSwap);
+}
+int main()
+
+{
+    vector<int>arr{6,4,8,7,0,-8};
+    recursiveBubble(6,arr,1);
+    for (int &it:arr)
+    cout<<it<<" ";
+}
 /*
 #include <bits/stdc++.h>
 
