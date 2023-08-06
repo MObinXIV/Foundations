1959386dfa9963a0121aae163ae214b35651b4bf
MObinXIV <mobinxiv@gmail.com>
2023-08-06 14:20:50 +0300

    use vector and modify it to handle negative numbers

--- concepts/Sorting/radixSort/radix.cpp
+++ concepts/Sorting/radixSort/radix.cpp
@@ -1,24 +1,33 @@
-// Solves the wasted space problem in counting sort
-// Sort the numbers according to every digit in
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
-int getMax(int arr[], int size)
+
+int getMax(vector<int> arr)
 {
     int max = arr[0];
-    for (int i = 1; i < size; i++)
+    for (int i = 1; i < arr.size(); i++)
         if (arr[i] > max)
             max = arr[i];
     return max;
 }
-void countingSort(int arr[], int size, int div)
-{
-    int output[size];
-    int count[10] = {0};
 
-    for (int i = 0; i < size; i++)
-        count[(arr[i] / div) % 10]++;// get the current digit and hash it into the array
-// Do the accumlative sum
+void countingSort(vector<int> arr, int div)
+{
+    vector<int> output(arr.size());
+    vector<int> count(10, 0);
+
+    int originalSize = arr.size();
+
+    for (int i = 0; i < originalSize; i++)
+    {
+        int temp = arr[i];
+        if (temp < 0)
+            temp = ~temp;
+        count[(temp / div) % 10]++; // get the current digit and hash it into the array
+    }
+
+    // Do the accumlative sum
     for (int i = 1; i < 10; i++)
         count[i] += count[i - 1];
 
@@ -30,27 +39,36 @@ void countingSort(int arr[], int size, int div)
     Decrement the count (since we are going backwards)
     Store the element arr[i] in the output array at the correct position based on the count
     */
-    for (int i = size - 1; i >= 0; i--)
+    for (int i = originalSize - 1; i >= 0; i--)
     {
-        output[count[(arr[i] / div) % 10] - 1] = arr[i];
-        count[(arr[i] / div) % 10]--;
+        int temp = arr[i];
+        if (temp < 0)
+            temp = ~temp;
+        output[count[(temp / div) % 10] - 1] = temp;
+        count[(temp / div) % 10]--;
     }
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < originalSize; i++)
         arr[i] = output[i];
 }
-void radixSort(int arr[], int size)
+
+void radixSort(vector<int> arr)
 {
-    int m = getMax(arr, size);
+    int m = getMax(arr);
 
     for (int div = 1; m / div > 0; div *= 10)
-        countingSort(arr, size, div);
+        countingSort(arr, div);
 }
-int main()
 
+int main()
 {
-    int arr[5] = {4, 1, 3, 5, 6};
-    radixSort(arr, 5);
-    for (int i = 0; i < 5; i++)
+    vector<int> arr = {-123, 0, 1, -789, 234, -123, -456, -789, 234};
+    radixSort(arr);
+
+    for (int i = 0; i < arr.size(); i++)
         cout << arr[i] << " ";
+
+    cout << endl;
+
+    return 0;
 }
