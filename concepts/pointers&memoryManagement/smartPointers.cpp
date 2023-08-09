614ecfdc804de954342ec06885f853baa63ac8fd
MObinXIV <mobinxiv@gmail.com>
2023-08-10 02:33:13 +0300

    use move to move the ownershipe between pointers

--- concepts/pointers&memoryManagement/smartPointers.cpp
+++ concepts/pointers&memoryManagement/smartPointers.cpp
@@ -26,3 +26,45 @@ int main()
         unique_ptr<MyClass> ptr(new MyClass());
     }
 }
+
+#include <iostream>
+#include <memory>
+
+class MyClass
+{
+public:
+    int i;
+
+    MyClass()
+    {
+        std::cout << "\n created\n";
+    }
+
+    ~MyClass()
+    {
+        std::cout << "\n destroyed\n";
+    }
+};
+
+int main()
+{
+    if (true)
+    {
+        // Scope of the following MyClass object is this if-block
+        std::unique_ptr<MyClass> ptr(new MyClass());
+
+        /*Uncommenting the following line will produce an error as
+        copying of unique pointers is not allowed */
+        // std::unique_ptr<MyClass> ptr2 = ptr;
+
+        // Instead move will safely transfer the object ownership from ptr to ptr2
+        std::unique_ptr<MyClass> ptr2 = std::move(ptr);
+        // Must not use the old pointer now.
+        ptr2->i = 5;
+        std::cout << "\n"
+                  << ptr2->i << "\n";
+    }
+    // The pointer gets deallocated automatically at this point.
+    // Thus, the destructor of MyClass is called here.
+    return 0;
+}
