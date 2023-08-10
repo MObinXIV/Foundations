c99fdc6492842b8cac2a5e3a7cb3bfd05eda5438
MObinXIV <mobinxiv@gmail.com>
2023-08-10 04:35:01 +0300

    cyclic  shared pointers

--- concepts/pointers&memoryManagement/smartPointers.cpp
+++ concepts/pointers&memoryManagement/smartPointers.cpp
@@ -109,3 +109,69 @@ int main()
 
     return 0;
 }
+
+//Smart pointers help reduce memory leaks and deallocate memory. However, they don’t completely eliminate the need for us to manually deallocate memory. For instance, in a resource-constrained device, we may need to manually free memory immediately after its last use, even before a pointer is out of scope.
+
+// Unique pointer is created
+std::unique_ptr<MyClass> ptr(new MyClass());
+
+// Some code here to use it.
+//. . .
+
+// When we do not need it, we can manually release and delete it.
+// After release pointer is not automatically managed
+MyClass *raw = ptr.release();
+// Manually delete it.
+delete raw;
+
+// While smart pointers reduce the likelihood of memory leaks, they don’t eliminate them entirely. For instance, if we use cyclic shared pointers, our reference count will never be zero and memory will never be automatically released. In such a situation, we must either avoid smart pointers altogether or resort to manually deallocating the smart pointers.
+
+//The following code example demonstrates the use of cyclic pointers, where shared pointers will not be able to deallocate memory automatically:
+
+#include <iostream>
+#include <memory>
+
+class Cyclic
+{
+public:
+    std::shared_ptr<Cyclic> myObj;
+    int k;
+
+    Cyclic(int j)
+    {
+        k = j;
+        std::cout << "\n created\n";
+    }
+
+    ~Cyclic()
+    {
+        std::cout << "\n destroyed\n";
+    }
+
+    void setObject(std::shared_ptr<Cyclic> obj)
+    {
+        myObj = obj;
+    }
+};
+
+//  ptr
+//  /   \
+//ptr2  ptr
+int main()
+{
+    if (true)
+    {
+        // First shared pointer
+        std::shared_ptr<Cyclic> ptr = std::make_shared<Cyclic>(5);
+
+        // Second shared pointer
+        std::shared_ptr<Cyclic> ptr2 = std::make_shared<Cyclic>(6);
+
+        // Creating cyclic dependencies.
+        ptr->setObject(ptr2);
+        ptr2->setObject(ptr);
+    }
+    // Even outside of scope the object continues to exist.
+    // Thus, destructor is never called and "destroyed" never get printed
+    return 0;
+}
